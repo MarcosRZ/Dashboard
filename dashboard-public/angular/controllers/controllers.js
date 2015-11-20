ec00ba43a871205d532bbf63ad1e8009708af258
MarcosRZ <msucaro@gmail.com>
2015-11-20 03:10:21 +0100

    Gauges now working with angular-justgage directives

--- dashboard-public/angular/controllers/controllers.js
+++ dashboard-public/angular/controllers/controllers.js
@@ -1,39 +1,19 @@
-var Controllers = angular.module('Controllers', ['Services'])
+var Controllers = angular.module('Controllers', ['Services', 'ngJustGage'])
 
 Controllers.controller('CpuController', ['$scope', '$interval', 'RamService', 'DiskService', function ($scope, $interval, RamService, DiskService) {
 
-        $scope.ram = {}
-        $scope.disk = {}
+        $scope.ram = {percent:50}
+        $scope.disk = {percent:50}
 
         $scope.updateRam = (ram) => {
             $scope.ram = ram;
             $scope.ram.percent = ((ram.totalmem - ram.freemem) * 100 / ram.totalmem);
-            $scope.ramGauge.refresh($scope.ram.percent);
-            console.log($scope.ram.percent)
         }
 
         $scope.updateDisk = (disk) => {
             $scope.disk = disk;
             $scope.disk.percent = ((disk.total - disk.free) * 100 / disk.total);
-            $scope.diskGauge.refresh($scope.disk.percent);
-            console.log($scope.disk.percent);
         }
-
-        $scope.ramGauge = new JustGage({
-            id: "ram-gauge",
-            formatNumber: false,
-            counter: false,
-            title: 'RAM',
-            label: '%'
-        });
-
-        $scope.diskGauge = new JustGage({
-            id: "disk-gauge",
-            formatNumber: false,
-            counter: false,
-            title: 'Disk',
-            label: '%'
-        });
 
         $scope.work = () => {
             RamService.get($scope.updateRam);
