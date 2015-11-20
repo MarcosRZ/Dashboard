53cdf9d1f85a9d2375f33ecc2d7790dc69831409
MarcosRZ <msucaro@gmail.com>
2015-11-20 02:07:28 +0100

    Added justGauge for fun

--- dashboard-public/angular/controllers/controllers.js
+++ dashboard-public/angular/controllers/controllers.js
@@ -7,32 +7,39 @@ Controllers.controller('CpuController', ['$scope', '$interval', 'RamService', 'D
 
         $scope.updateRam = (ram) => {
             $scope.ram = ram;
-            $scope.ram.mempercent = ((ram.totalmem - ram.freemem) * 100 / ram.totalmem);
-            $scope.gg1.refresh($scope.ram.mempercent);
-            $scope.gg1.label = $scope.ram.used + '/' + $scope.ram.total;
+            $scope.ram.percent = ((ram.totalmem - ram.freemem) * 100 / ram.totalmem);
+            $scope.ramGauge.refresh($scope.ram.percent);
+            console.log($scope.ram.percent)
         }
 
         $scope.updateDisk = (disk) => {
             $scope.disk = disk;
-            $scope.disk.percent = disk.available * 100 / disk.total;
+            $scope.disk.percent = ((disk.total - disk.free) * 100 / disk.total);
+            $scope.diskGauge.refresh($scope.disk.percent);
+            console.log($scope.disk.percent);
         }
-        
+
+        $scope.ramGauge = new JustGage({
+            id: "ram-gauge",
+            formatNumber: false,
+            counter: false,
+            title: 'RAM',
+            label: '%'
+        });
+
+        $scope.diskGauge = new JustGage({
+            id: "disk-gauge",
+            formatNumber: false,
+            counter: false,
+            title: 'Disk',
+            label: '%'
+        });
+
         $scope.work = () => {
             RamService.get($scope.updateRam);
-           // DiskService.get($scope.updateDisk);
+            DiskService.get($scope.updateDisk);
         }
 
         $interval($scope.work, 1000);
-
-        $scope.gg1 = new JustGage({
-            id: "gg1",
-            formatNumber: false,
-            counter: true,
-            title: 'Ram',
-            label: $scope.ram.used + '/' + $scope.ram.total
-        });
-
-        
-    
     }
 ]);
